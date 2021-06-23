ebd0739c49ecaa47f3fd5158531e88ff9dc3ea3d
leeviko <lerppana0009@gmail.com>
2021-06-23 15:38:26 +0300

    initial commit

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = process.env.MONGO_URI
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      db,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+      }
+    )
+    console.log("MongoDB is connected");
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+module.exports = connectDB;
